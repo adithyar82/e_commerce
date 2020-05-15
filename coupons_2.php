91271a7914065f9e2f1288946916c72989a58ef0
Aditya2657 <maditya183@gmail.com>
2020-05-15 18:43:44 +0530

    coupon_page

--- coupons_2.php
+++ coupons_2.php
@@ -1,4 +1,7 @@
 <?php 
+echo'<script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert-dev.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.css">';
 include('connect_db.php');
 if(!isset($_SESSION['uname'])){
     header("location:index.php");
@@ -12,8 +15,18 @@ if(isset($_POST['submit'])){
     $result = $conn->query($sql);
     if($result->num_rows>=0){
         echo '<script>
-        alert("Registerd Successfully")
-        window.location = "coupons_1.php";
+        setTimeout(function () { 
+            swal({
+            title: "Confermation",
+            text: "Coupond added successfully",
+            type: "success",
+            confirmButtonText: "OK"
+            },
+            function(isConfirm){
+            if (isConfirm) {
+                window.location.href = "coupons_1.php";;
+            }
+            }); }, 1000);
         </script>';
     }
 }
